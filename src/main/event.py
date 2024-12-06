58aa5dbc393360c6cecb41babd6a4091a451c3bb
nepia0318 <49799401+nepia0318@users.noreply.github.com>
2024-12-07 05:14:17 +0900

    Add rate difference between previous day and current day

--- src/main/event.py
+++ src/main/event.py
@@ -4,30 +4,25 @@ import discord
 from .scraping import getCurrentRateByUserId
 
 async def kamlon(ctx):
-    KAMLON_ID=54285
+    KAMLON_ID = 54285
     try:
         data = getCurrentRateByUserId(KAMLON_ID)
         embedMsg = discord.Embed(
             title='かむろん',
             description=f'\
                 現在レート: {data["current_rate"]}\n\
+                前日比　　: {data["difference_rate"]}\n\
                 最高レート: {data["maximum_rate"]}\n\
                 対戦成績　: {data["match_result"]}',
             color=discord.Colour.green()
         )
-        # embedMsg.add_field(name="test", value='')
 
         await ctx.send(embed=embedMsg)
 
-        # await message.channel.send(
-        #     'かむろんさん\n'\
-        #     f'現在のレート: {currentRate}'\
-        # )
-
     except requests.RequestException as e:
-        print(f"リクエストエラー: {e}")
+        print(f"Error: {e}")
         await ctx.send('取得に失敗しました')
 
     except Exception as e:
-        print(f"エラーが発生しました: {e}")
+        print(f"Error: {e}")
         await ctx.send('取得に失敗しました')
--- src/main/scraping.py
+++ src/main/scraping.py
@@ -1,29 +1,34 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 def getCurrentRateByUserId(userId):
-    baseUrl = "https://smashmate.net/user/"
+    base_url = "https://smashmate.net/user/"
+    print(f'url: {base_url + str(userId)}')
 
-    # ウェブページの内容を取得
-    response = requests.get(baseUrl + str(userId))
-    response.raise_for_status()  # エラーがあれば例外を発生させる
+    # Get Webpage content
+    response = requests.get(base_url + str(userId))
+    response.raise_for_status()  # Throw exception if request failed
 
-    # BeautifulSoupでHTMLを解析
+    # Parse HTML
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # 'current-rate'クラスを持つ要素を探す
+    # Find current-rate class
     elements = soup.select('div.side > div.row.row-center.va-middle > div.col-xs-6')
 
     results = {}
     if elements:
+        current_rate    = re.search(r'\d+', elements[1].text.strip())
+        difference_rate = re.search(r'(?<=前日比：)-*\d+', elements[1].text.strip())
+
         results = {
-            'current_rate': elements[1].text.strip(),
-            'maximum_rate': elements[3].text.strip(),
-            'match_result': elements[5].text.strip()
+            'current_rate'   : current_rate.group() if current_rate is not None else 'None' ,
+            'difference_rate': difference_rate.group() if difference_rate is not None else 'None',
+            'maximum_rate'   : elements[3].text.strip(),
+            'match_result'   : elements[5].text.strip()
         }
         print(results)
     else:
-        print("合致する情報が取得できませんでした")
-        raise Exception
+        raise Exception("Data not found.")
 
     return results
